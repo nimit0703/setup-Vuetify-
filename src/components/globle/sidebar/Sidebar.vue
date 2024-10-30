16ec0b6c074820b0ba283e5293bc38516823c3a5
nimit0703 <nimit.190410107143@gmail.com>
2024-10-30 12:05:36 +0530

    SETUP

--- src/components/globle/sidebar/Sidebar.vue
+++ src/components/globle/sidebar/Sidebar.vue
@@ -1,46 +1,57 @@
 <template>
-    <v-card>
-        <v-layout>
-            <v-navigation-drawer expand-on-hover @update:rail="toggle" rail>
-                <v-list>
-                    <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-                        subtitle="sandra_a88@gmailcom" title="Sandra Adams"></v-list-item>
-                </v-list>
+    <v-app>
+        <div class="d-lg-none d-xl-flex">
+            <v-app-bar title="">
+                <v-app-bar-title>Mobile Navbar</v-app-bar-title>
 
-                <v-divider></v-divider>
+                <template v-slot:append>
+                    <v-btn @click="toggleTheme"
+                        :icon="isDarkTheme ? 'mdi-lightbulb-night-outline' : 'mdi-lightbulb-night'"></v-btn>
+                    <v-btn icon="mdi-account-multiple"></v-btn>
 
-                <v-list density="compact" nav>
-                    <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me"
-                        value="shared"></v-list-item>
-                    <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
-                    <v-list-item :prepend-icon="isDarkTheme ? 'mdi-toggle-switch' : 'mdi-toggle-switch-off-outline'"
-                        title="Toggle" value="toggle" @click="toggleTheme"></v-list-item>
-                </v-list>
-            </v-navigation-drawer>
+                    <v-btn icon="mdi-star"></v-btn>
+                    <v-avatar image="https://randomuser.me/api/portraits/women/85.jpg" size="small" class="me-2">
+                        
+                    </v-avatar>
 
-            <v-main :style="drawer ? 'margin-left:216px' : ''" style="height: 100vh;">
-                <v-alert 
-                 v-if="alertDetails.showAlert" 
-                    :closable="alertDetails.closable" 
-                    :icon="alertDetails.icon"
-                    :title="alertDetails.title" 
-                    :text="alertDetails.text" 
-                    :variant="alertDetails.variant"
-                    :type="alertDetails.type"
-                    :color="alertDetails.type"
-                    @click:close="closeAlert"
-                >
-                </v-alert>
-                <div class="px-5 border overflow-y-scroll no-scrollbar" style="height: inherit;">
-                    <router-view class="px-5"/>
-                </div>
-            </v-main>
-        </v-layout>
-    </v-card>
+                </template>
+            </v-app-bar>
+        </div>
+        <v-card>
+            <v-layout>
+                <v-navigation-drawer expand-on-hover @update:rail="toggle" rail>
+                    <v-list>
+                        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+                            subtitle="sandra_a88@gmailcom" title="Sandra Adams"></v-list-item>
+                    </v-list>
+
+                    <v-divider></v-divider>
+
+                    <v-list density="compact" nav>
+                        <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me"
+                            value="shared"></v-list-item>
+                        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+                        <v-list-item :prepend-icon="isDarkTheme ? 'mdi-toggle-switch' : 'mdi-toggle-switch-off-outline'"
+                            title="Toggle" value="toggle" @click="toggleTheme"></v-list-item>
+                    </v-list>
+                </v-navigation-drawer>
+
+                <v-main :style="drawer ? 'margin-left:216px' : ''" style="height: 100vh;">
+                    <v-alert v-if="alertDetails.showAlert" :closable="alertDetails.closable" :icon="alertDetails.icon"
+                        :title="alertDetails.title" :text="alertDetails.text" :variant="alertDetails.variant"
+                        :type="alertDetails.type" :color="alertDetails.type" @click:close="closeAlert">
+                    </v-alert>
+                    <div class="px-5 border overflow-y-scroll no-scrollbar" style="height: inherit;">
+                        <router-view class="px-5" />
+                    </div>
+                </v-main>
+            </v-layout>
+        </v-card>
+    </v-app>
 </template>
 
 <script setup>
-import { ref ,computed } from 'vue';
+import { ref, computed } from 'vue';
 import { useTheme } from 'vuetify'
 import { useStore } from 'vuex';
 
@@ -61,18 +72,20 @@ function toggleTheme() {
 const alertDetails = computed(() => store.getters.getAlertDetails);
 
 const closeAlert = () => {
-  store.dispatch('updateAlertDetails', { showAlert: false });
+    store.dispatch('updateAlertDetails', { showAlert: false });
 };
 </script>
 
 <style>
 .no-scrollbar {
-  scrollbar-width: none; /* Firefox */
-  -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none;
+    /* Firefox */
+    -ms-overflow-style: none;
+    /* IE and Edge */
 }
 
 .no-scrollbar::-webkit-scrollbar {
-  display: none; /* Chrome, Safari, and Opera */
+    display: none;
+    /* Chrome, Safari, and Opera */
 }
 </style>
-
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -4,7 +4,7 @@
       src="https://images.unsplash.com/photo-1610241436112-b76fa54258c5?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       height="500">
       <div class="d-flex flex-column fill-height justify-center align-center text-white">
-        <h1 class="text-h4 font-weight-thin mb-4 " >
+        <h1 class="text-h4 font-weight-thin mb-4 ">
           Nimit Trevadiya
         </h1>
         <h4 class="subheading">
@@ -41,14 +41,13 @@
     </v-container-fluid>
 
     <v-parallax
-      src="https://images.unsplash.com/photo-1610241436112-b76fa54258c5?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      height="500">
-      <div class="d-flex flex-column fill-height justify-center align-center text-white">
+      src="https://images.unsplash.com/photo-1610241436112-b76fa54258c5?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
+      <div class="d-flex flex-column fill-height justify-center align-left text-white ml-5">
         <h1 class="text-h4 font-weight-thin mb-4">
-          This is Footer area
-        </h1>
-        <h4 class="subheading">
           Welcomes with the cool setup!
+        </h1>
+        <h4 class="subheading text-wrap " style="max-width: 336px;">
+          This is just starting of the coolest components now wait and scroll
         </h4>
       </div>
     </v-parallax>
